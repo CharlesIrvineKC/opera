eb059f7a31ad0426025a62ed71e7251a3217a70a
Chuck Irvine <chuck.irvine@icloud.com>
2024-07-20 17:52:18 -0500

    process starting from GUI

--- lib/opera_web/live/tasks_live.ex
+++ lib/opera_web/live/tasks_live.ex
@@ -2,10 +2,12 @@ defmodule OperaWeb.TasksLive do
   use OperaWeb, :live_view
 
   alias Mozart.ProcessService
+  alias Mozart.ProcessEngine
 
   def mount(_params, _session, socket) do
     user_tasks = ProcessService.get_user_tasks()
-    process_names = Enum.map(ProcessService.get_process_models(), fn {k,_v} -> k end)
+    process_names = Enum.map(ProcessService.get_process_models(), fn {k, _v} -> k end)
+
     socket =
       assign(socket,
         user_tasks: user_tasks,
@@ -18,7 +20,7 @@ defmodule OperaWeb.TasksLive do
 
   def render(assigns) do
     ~H"""
-    <.bottom_nav process_names={@process_names}/>
+    <.bottom_nav process_names={@process_names} />
     <div class="mx-10 flex flex-row">
       <div>
         <.user_task_ref
@@ -44,8 +46,8 @@ defmodule OperaWeb.TasksLive do
           <ul class="flex flex-row font-medium mt-0 space-x-8 rtl:space-x-reverse text-sm">
             <li>
               <button
-                data-modal-target="authentication-modal"
-                data-modal-toggle="authentication-modal"
+                data-modal-target="process-start-modal"
+                data-modal-toggle="process-start-modal"
                 class="block text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
                 type="button"
               >
@@ -56,14 +58,14 @@ defmodule OperaWeb.TasksLive do
         </div>
       </div>
     </nav>
-    <.choose_process process_names={@process_names}/>
+    <.choose_process process_names={@process_names} />
     """
   end
 
   def choose_process(assigns) do
     ~H"""
     <div
-      id="authentication-modal"
+      id="process-start-modal"
       tabindex="-1"
       aria-hidden="true"
       class="hidden overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full"
@@ -79,7 +81,7 @@ defmodule OperaWeb.TasksLive do
             <button
               type="button"
               class="end-2.5 text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white"
-              data-modal-hide="authentication-modal"
+              data-modal-hide="process-start-modal"
             >
               <svg
                 class="w-3 h-3"
@@ -101,15 +103,22 @@ defmodule OperaWeb.TasksLive do
           </div>
           <!-- Modal body -->
           <div class="p-4 md:p-5">
-            <form class="max-w-sm mx-auto">
+            <form phx-submit="start_process" class="max-w-sm mx-auto">
               <select
-                id="countries"
+                id="process"
+                name="process"
                 class="mt-2 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
               >
                 <option selected>Choose a process</option>
                 <option :for={name <- @process_names} value={name}><%= name %></option>
               </select>
-              <button type="button" class="mt-6 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Start</button>
+              <button
+                data-modal-hide="process-start-modal"
+                type="submit"
+                class="mt-6 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
+              >
+                Start
+              </button>
             </form>
           </div>
         </div>
@@ -145,10 +154,10 @@ defmodule OperaWeb.TasksLive do
       <div class="grid grid-cols-3 gap-6 mb-8">
         <.output_field :for={name <- @current_task.outputs} name={name} value="" />
       </div>
-      <div class="flex flex-col items-center">
+      <div class="flex gap-2 flex-row">
         <button
           type="submit"
-          class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm text-center px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+          class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm text-center px-5 py-2.5 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
         >
           Submit
         </button>
@@ -203,5 +212,97 @@ defmodule OperaWeb.TasksLive do
     Process.sleep(500)
     user_tasks = ProcessService.get_user_tasks()
     {:noreply, assign(socket, user_tasks: user_tasks, current_task: nil)}
+  end
+
+  def handle_event("start_process", %{"process" => process_name}, socket) do
+    customer = %{"Name" => "Bob Dylan", "Income" => 5_000_000, "Debt" => 0}
+
+    {:ok, ppid, _uid, _key} =
+      ProcessEngine.start_process(process_name, customer, customer["Name"])
+
+    ProcessEngine.execute(ppid)
+    Process.sleep(500)
+    user_tasks = ProcessService.get_user_tasks()
+    {:noreply, assign(socket, user_tasks: user_tasks, current_task: nil)}
+  end
+
+  def name(assigns) do
+    ~H"""
+    <button
+      data-modal-target="popup-modal"
+      data-modal-toggle="popup-modal"
+      class="block text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+      type="button"
+    >
+      Toggle modal
+    </button>
+
+    <div
+      id="popup-modal"
+      tabindex="-1"
+      class="hidden overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full"
+    >
+      <div class="relative p-4 w-full max-w-md max-h-full">
+        <div class="relative bg-white rounded-lg shadow dark:bg-gray-700">
+          <button
+            type="button"
+            class="absolute top-3 end-2.5 text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white"
+            data-modal-hide="popup-modal"
+          >
+            <svg
+              class="w-3 h-3"
+              aria-hidden="true"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 14 14"
+            >
+              <path
+                stroke="currentColor"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"
+              />
+            </svg>
+            <span class="sr-only">Close modal</span>
+          </button>
+          <div class="p-4 md:p-5 text-center">
+            <svg
+              class="mx-auto mb-4 text-gray-400 w-12 h-12 dark:text-gray-200"
+              aria-hidden="true"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 20 20"
+            >
+              <path
+                stroke="currentColor"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                d="M10 11V6m0 8h.01M19 10a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"
+              />
+            </svg>
+            <h3 class="mb-5 text-lg font-normal text-gray-500 dark:text-gray-400">
+              Are you sure you want to delete this product?
+            </h3>
+            <button
+              data-modal-hide="popup-modal"
+              type="button"
+              class="text-white bg-red-600 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 dark:focus:ring-red-800 font-medium rounded-lg text-sm inline-flex items-center px-5 py-2.5 text-center"
+            >
+              Yes, I'm sure
+            </button>
+            <button
+              data-modal-hide="popup-modal"
+              type="button"
+              class="py-2.5 px-5 ms-3 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700"
+            >
+              No, cancel
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+    """
   end
 end
--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -1,17 +1,1 @@
-alias Mozart.ProcessEngine
-
 Opera.Processes.HomeLoanApp.load()
-
-customers = [
-  %{"Name" => "John Doe", "Income" => 50_000, "Debt" => 10_000},
-  %{"Name" => "Jane Doe", "Income" => 60_000, "Debt" => 10_000},
-  %{"Name" => "Robert Smith", "Income" => 80_000, "Debt" => 10_000},
-  %{"Name" => "Sallie Long", "Income" => 120_000, "Debt" => 10_000},
-  %{"Name" => "Mary Winn", "Income" => 500_000, "Debt" => 10_000}
-]
-
-Enum.each(customers, fn customer ->
-  {:ok, ppid, _uid, _key} = ProcessEngine.start_process("Home Loan", customer, customer["Name"])
-  ProcessEngine.execute(ppid)
-  Process.sleep(50)
-end)
