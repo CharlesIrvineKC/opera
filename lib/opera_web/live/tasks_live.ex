713c973f38d5488b64fe001dff96483dbd488416
Chuck Irvine <chuck.irvine@icloud.com>
2024-07-20 13:24:39 -0500

    move top nav to app.html.heex

--- lib/opera_web/live/tasks_live.ex
+++ lib/opera_web/live/tasks_live.ex
@@ -18,7 +18,6 @@ defmodule OperaWeb.TasksLive do
 
   def render(assigns) do
     ~H"""
-    <.top_nav />
     <.bottom_nav process_names={@process_names}/>
     <div class="mx-10 flex flex-row">
       <div>
@@ -34,24 +33,6 @@ defmodule OperaWeb.TasksLive do
         <.task_form current_task={@current_task} />
       </div>
     </div>
-    """
-  end
-
-  def top_nav(assigns) do
-    ~H"""
-    <nav class="bg-white border-gray-200 dark:bg-gray-900">
-      <div class="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl p-4">
-        <a href="#" class="flex items-center space-x-3 rtl:space-x-reverse">
-          <img src="https://flowbite.com/docs/images/logo.svg" class="h-8" alt="Flowbite Logo" />
-          <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
-            Opera
-          </span>
-        </a>
-        <div class="flex items-center space-x-6 rtl:space-x-reverse">
-          <a href="#" class="text-sm  text-blue-600 dark:text-blue-500 hover:underline">Login</a>
-        </div>
-      </div>
-    </nav>
     """
   end
 
