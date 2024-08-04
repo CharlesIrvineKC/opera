10115dd2a7d5f516c4b55d7bb7b41c3ee828b8bf
Chuck Irvine <chuck.irvine@icloud.com>
2024-08-04 10:28:08 -0500

    allow clearing of all state

--- lib/opera_web/live/processes_live.ex
+++ lib/opera_web/live/processes_live.ex
@@ -100,6 +100,8 @@ defmodule OperaWeb.ProcessesLive do
               </div>
             </li>
           </ul>
+
+
           <ul class="flex flex-col font-medium p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
             <li>
               <button
@@ -149,6 +151,56 @@ defmodule OperaWeb.ProcessesLive do
                       class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
                     >
                       Completed Processes
+                    </a>
+                  </li>
+                </ul>
+              </div>
+            </li>
+          </ul>
+
+
+
+
+          <ul class="flex flex-col font-medium p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+            <li>
+              <button
+                id="adminNavBarLink"
+                data-dropdown-toggle="adminNavBar"
+                class="flex items-center justify-between w-full py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 md:w-auto dark:text-white md:dark:hover:text-blue-500 dark:focus:text-white dark:border-gray-700 dark:hover:bg-gray-700 md:dark:hover:bg-transparent"
+              >
+                Admin
+                <svg
+                  class="w-2.5 h-2.5 ms-2.5"
+                  aria-hidden="true"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 10 6"
+                >
+                  <path
+                    stroke="currentColor"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    stroke-width="2"
+                    d="m1 1 4 4 4-4"
+                  />
+                </svg>
+              </button>
+              <!-- Dropdown menu -->
+              <div
+                id="adminNavBar"
+                class="z-10 hidden font-normal bg-white divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-gray-700 dark:divide-gray-600"
+              >
+                <ul
+                  class="py-2 text-sm text-gray-700 dark:text-gray-400"
+                  aria-labelledby="dropdownLargeButton"
+                >
+                  <li>
+                    <a
+                      phx-click="clear-databases"
+                      href="#"
+                      class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
+                    >
+                      Clear Databases
                     </a>
                   </li>
                 </ul>
@@ -327,6 +379,11 @@ defmodule OperaWeb.ProcessesLive do
     module = Module.concat(Elixir, String.to_atom(module_name))
     apply(module, :load, [])
     {:noreply, socket}
+  end
+
+  def handle_event("clear-databases", _params, socket) do
+    ProcessService.clear_state()
+    {:noreply, redirect(socket, to: ~p"/processes")}
   end
 
   def handle_event("show-process", %{"process-id" => process_uid}, socket) do
