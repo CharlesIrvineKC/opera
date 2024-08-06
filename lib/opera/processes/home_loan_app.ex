b7ab3843609fda273802c6fa3d3c980e84d72101
Chuck Irvine <chuck.irvine@icloud.com>
2024-08-06 17:59:43 -0500

    bump mozart version, add exception_task

--- lib/opera/processes/home_loan_app.ex
+++ lib/opera/processes/home_loan_app.ex
@@ -4,12 +4,8 @@ defmodule Opera.Processes.HomeLoanApp do
 
   def_bpm_application("Home Loan Process", main: "Home Loan", data: "Customer Name,Income,Debt")
 
-  def pre_approved(data) do
-    data["Pre Approval"] == "Approved"
-  end
-
   def pre_approval_declined(data) do
-   data["Pre Approval"] == "Declined"
+    data["Pre Approval"] == "Declined"
   end
 
   def_choice_type("Pre Approval", choices: "Approved, Declined")
@@ -18,60 +14,44 @@ defmodule Opera.Processes.HomeLoanApp do
 
   defprocess "Home Loan" do
     user_task("Perform Pre-Approval", groups: "credit", outputs: "Pre Approval")
-    case_task "Route on Pre-Approcal Completion" do
-      case_i :pre_approved do
-        user_task("Receive Mortage Application", groups: "credit", outputs: "Purchase Price")
-        user_task("Process Loan", groups: "credit", outputs: "Loan Verified")
-        subprocess_task("Perform Loan Evaluation", model: "Perform Loan Evaluation Process")
-      end
-      case_i :pre_approval_declined do
-          user_task("Communicate Loan Denied", groups: "credit", outputs: "Communicate Loan Denied")
-      end
-    end
-  end
 
-
-def loan_verified(data) do
-  data["Loan Verified"] == "Pass"
-end
-
-def loan_failed_verification(data) do
-  data["Loan Verified"] == "Fail"
-end
-
-defprocess "Perform Loan Evaluation Process" do
-  case_task "Process Loan Outcome" do
-    case_i :loan_verified do
-      user_task("Perform Underwriting", groups: "underwriting", outputs: "Loan Approved")
-      subprocess_task("Route from Underwriting", model: "Route from Underwriting Process")
-    end
-    case_i :loan_failed_verification do
+    exception_task "Pre-Approval Denied", :pre_approval_declined do
       user_task("Communicate Loan Denied", groups: "credit", outputs: "Communicate Loan Denied")
     end
+
+    user_task("Receive Mortage Application", groups: "credit", outputs: "Purchase Price")
+    user_task("Process Loan", groups: "credit", outputs: "Loan Verified")
+    subprocess_task("Perform Loan Evaluation", model: "Perform Loan Evaluation Process")
   end
-end
 
-def loan_approved(data) do
-  data["Loan Approved"] == "Approved"
-end
-
-def loan_declined(data) do
-  data["Loan Approved"] == "Declined"
-end
-
-def_choice_type("Communicate Loan Approved", choices: "By Phone, By US Mail")
-
-def_choice_type("Communicate Loan Denied", choices: "By Phone, By US Mail")
-
-defprocess "Route from Underwriting Process" do
-  case_task "Route from Underwriting" do
-    case_i :loan_approved do
-      user_task("Communicate Approval", groups: "credit", outputs: "Communicate Loan Approved")
-    end
-    case_i :loan_declined do
-      user_task("Communicate Loan Denied", groups: "customer_service", outputs: "Communicate Loan Denied")
-    end
+  def loan_failed_verification(data) do
+    data["Loan Verified"] == "Fail"
   end
-end
 
+  defprocess "Perform Loan Evaluation Process" do
+    exception_task "Loan Failed Verification", :loan_failed_verification do
+      user_task("Communicate Loan Denied", groups: "credit", outputs: "Communicate Loan Denied")
+    end
+
+    user_task("Perform Underwriting", groups: "underwriting", outputs: "Loan Approved")
+    subprocess_task("Route from Underwriting", model: "Route from Underwriting Process")
+  end
+
+  def loan_declined(data) do
+    data["Loan Approved"] == "Declined"
+  end
+
+  def_choice_type("Communicate Loan Approved", choices: "By Phone, By US Mail")
+
+  def_choice_type("Communicate Loan Denied", choices: "By Phone, By US Mail")
+
+  defprocess "Route from Underwriting Process" do
+    exception_task "Loan Declined", :loan_declined do
+      user_task("Communicate Loan Denied",
+        groups: "customer_service",
+        outputs: "Communicate Loan Denied"
+      )
+    end
+    user_task("Communicate Approval", groups: "credit", outputs: "Communicate Loan Approved")
+  end
 end
