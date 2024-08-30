643c9a2e187e991711e269af7cde4c9873b5a5fa
Chuck Irvine <chuck.irvine@icloud.com>
2024-08-29 21:08:30 -0500

    added a task listener to "Review Invoice"

--- lib/opera/processes/invoice_receipt.ex
+++ lib/opera/processes/invoice_receipt.ex
@@ -24,7 +24,7 @@ defmodule Opera.Processes.InvoiceReceipt do
 
   def negotiation_not_resolved(data) do
     data["Invoice Review Determination"] != "Rejected" &&
-    data["Invoice Approved?"] != "Approved"
+      data["Invoice Approved?"] != "Approved"
   end
 
   defprocess "Invoice Receipt Process" do
@@ -35,6 +35,7 @@ defmodule Opera.Processes.InvoiceReceipt do
       case_i :invoice_approved do
         subprocess_task("Perform Bank Transfer SubTask", model: "Perform Bank Transfer")
       end
+
       case_i :invoice_sent_to_review do
         subprocess_task("Perform Invoice Approval Negotiation Subprocess",
           model: "Perform Invoice Approval Negotiation"
@@ -51,6 +52,7 @@ defmodule Opera.Processes.InvoiceReceipt do
   defprocess "Perform Invoice Approval Negotiation" do
     repeat_task "Invoice Approval Negotiation", condition: :negotiation_not_resolved do
       subprocess_task("Review Invoice Subprocess", model: "Review Invoice Process")
+
       conditional_task "Reapprove if not Rejected", condition: :invoice_not_rejected do
         user_task("Reapprove Invoice", groups: "Admin", outputs: "Invoice Approved?")
       end
@@ -61,8 +63,17 @@ defmodule Opera.Processes.InvoiceReceipt do
     end
   end
 
+  def assign_user(user_task, data) do
+    Map.put(user_task, :assigned_user, data["Invoice Reviewer ID"])
+  end
+
   defprocess "Review Invoice Process" do
     user_task("Assign Reviewer", groups: "Admin", outputs: "Invoice Reviewer ID")
-    user_task("Review Invoice", groups: "Admin", outputs: "Invoice Review Determination")
+
+    user_task("Review Invoice",
+      groups: "Admin",
+      outputs: "Invoice Review Determination",
+      listener: :assign_user
+    )
   end
 end
