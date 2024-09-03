d26baa5af548b4a60cca0858e5337f64e2bbec13
Chuck Irvine <chuck.irvine@icloud.com>
2024-09-02 21:50:42 -0500

    bug fix

--- lib/opera_web/form_helper.ex
+++ lib/opera_web/form_helper.ex
@@ -1,26 +1,39 @@
 defmodule OperaWeb.FormHelper do
-  @input_field_ordering %{Opera.Processes.HomeLoanApp => [
-    "First Name",
-    "Last Name",
-    "Debt",
-    "Income"
-  ]
-}
+  @input_field_ordering %{
+    Opera.Processes.HomeLoanApp => [
+      "First Name",
+      "Last Name",
+      "Debt",
+      "Income"
+    ],
+    Opera.Processes.InvoiceReceipt => [
+      "First Name",
+      "Last Name"
+    ]
+  }
 
   def get_ordered_inputs(module, data) do
-    ordered_inputs =
-      Enum.reduce(@input_field_ordering[module], [], fn field, acc ->
-        value = Map.get(data, field)
-        if value, do: acc ++ [{field, value}], else: acc
-      end)
-    data =
-      Enum.reduce(data, [], fn {k,v}, acc ->
-        if Enum.find(ordered_inputs, fn {k1, _v1} -> k == k1 end) do
-          acc
-        else
-          [{k,v} | acc]
-        end
-      end)
+    ordering = @input_field_ordering[module]
+
+    if ordering do
+      ordered_inputs =
+        Enum.reduce(ordering, [], fn field, acc ->
+          value = Map.get(data, field)
+          if value, do: acc ++ [{field, value}], else: acc
+        end)
+
+      data =
+        Enum.reduce(data, [], fn {k, v}, acc ->
+          if Enum.find(ordered_inputs, fn {k1, _v1} -> k == k1 end) do
+            acc
+          else
+            [{k, v} | acc]
+          end
+        end)
+
       ordered_inputs ++ data
+    else
+      data
+    end
   end
 end
