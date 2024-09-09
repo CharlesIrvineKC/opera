2dce41d1e23e2f7353ffe556b5b2ee15cda3016a
Chuck Irvine <chuck.irvine@icloud.com>
2024-09-09 16:45:14 -0500

    allow user to toggle out of starting a process

--- lib/opera_web/live/tasks_live.ex
+++ lib/opera_web/live/tasks_live.ex
@@ -56,7 +56,8 @@ defmodule OperaWeb.TasksLive do
     ~H"""
     <button
       id="startBpmAppButton"
-      phx-click={JS.remove_class("hidden", to: "#dropdown")}
+      phx-click={JS.toggle_class("hidden", to: "#dropdown")}
+      phx-click-away={JS.toggle_class("hidden", to: "#dropdown")}
       class="ml-4 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
       type="button"
     >
@@ -106,7 +107,7 @@ defmodule OperaWeb.TasksLive do
 
   def handle_select_app(js \\ %JS{}) do
     js
-    |> JS.add_class("hidden", to: "#dropdown")
+    |> JS.toggle_class("hidden", to: "#dropdown")
     |> JS.push("set_current_app")
   end
 
