1823ac62cbd9e9701386f2858e0c83b530b1355c
Chuck Irvine <chuck.irvine@icloud.com>
2024-09-09 16:53:55 -0500

    BF: clicking outside of menu toggled menu

--- lib/opera_web/live/tasks_live.ex
+++ lib/opera_web/live/tasks_live.ex
@@ -57,7 +57,7 @@ defmodule OperaWeb.TasksLive do
     <button
       id="startBpmAppButton"
       phx-click={JS.toggle_class("hidden", to: "#dropdown")}
-      phx-click-away={JS.toggle_class("hidden", to: "#dropdown")}
+      phx-click-away={JS.add_class("hidden", to: "#dropdown")}
       class="ml-4 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
       type="button"
     >
