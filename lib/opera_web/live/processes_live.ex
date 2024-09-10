a25c0470d798bdebcbca70f4d7559fd42796082b
Chuck Irvine <chuck.irvine@icloud.com>
2024-09-10 15:13:13 -0500

    implement click-away to close menu

--- lib/opera_web/live/processes_live.ex
+++ lib/opera_web/live/processes_live.ex
@@ -132,6 +132,7 @@ defmodule OperaWeb.ProcessesLive do
         <button
           id="applicationsNavBarLink"
           phx-click={JS.remove_class("hidden", to: "#applicationsNavBar")}
+          phx-click-away={JS.add_class("hidden", to: "#applicationsNavBar")}
           class="flex items-center justify-between w-full py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 md:w-auto dark:text-white md:dark:hover:text-blue-500 dark:focus:text-white dark:border-gray-700 dark:hover:bg-gray-700 md:dark:hover:bg-transparent"
         >
           Load BPM Application
