c08e476233d5d15e8b922c5fe7de2f77729343fe
Chuck Irvine <chuck.irvine@icloud.com>
2024-09-15 09:52:15 -0500

    BF: history button was toggling task assignment

--- lib/opera_web/live/tasks_live.ex
+++ lib/opera_web/live/tasks_live.ex
@@ -291,8 +291,6 @@ defmodule OperaWeb.TasksLive do
         <span><%= @current_task.business_key %></span>
         <div>
           <button
-            phx-click="toggle-claim"
-            phx-value-task-uid={@current_task.uid}
             type="button"
             class="px-3 py-2 text-xs font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
           >
