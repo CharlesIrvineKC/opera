1aed2decc69c619352314d44266939312f65e640
Chuck Irvine <chuck.irvine@icloud.com>
2024-09-22 23:16:46 -0500

    BF: arg should have been the process name. not the process struct

--- lib/opera/workers/send_invoices_worker.ex
+++ lib/opera/workers/send_invoices_worker.ex
@@ -4,34 +4,36 @@ defmodule Opera.Workers.SendInvoicesWorker do
   alias Mozart.ProcessEngine
   alias Mozart.ProcessService
 
+  require Logger
+
   @impl Oban.Worker
   def perform(_job) do
-    process_model = ProcessService.get_process_model("Send Invoices")
+    try do
+      process_model = ProcessService.get_process_model("Send Invoices")
 
-    if process_model do
-      data = %{
-        "Company Name" => "Acme Hardware",
-        "Account Number" => "Acme 0001",
-        "Amount" => "10000"
-      }
+      if process_model do
+        data = %{
+          "Company Name" => "Acme Hardware",
+          "Account Number" => "Acme 0001",
+          "Amount" => "10000"
+        }
 
-      time = Timex.now() |> Timex.format!("{YYYY}-{0M}-{D}-{h24}-{m}-{s}")
+        time = Timex.now() |> Timex.format!("{YYYY}-{0M}-{D}-{h24}-{m}-{s}")
+        business_key = "Acme Hardware - 10000" <> "-" <> time
 
-      business_key = "Acme Hardware - 10000" <> "-" <> time
+        {:ok, ppid, _uid, _key} =
+          ProcessEngine.start_process(process_model.name, data, business_key)
 
-      {:ok, ppid, _uid, _key} =
-        ProcessEngine.start_process(process_model, data, business_key)
-
-      ProcessEngine.execute(ppid)
-      :ok
-    else
-      IO.puts("Application Send Invoices is not loaded or defined.")
-      :ok
+        ProcessEngine.execute(ppid)
+        :ok
+      else
+        IO.puts("Application Send Invoices is not loaded or defined.")
+        :ok
+      end
+    rescue
+      e ->
+        Logger.error("Job failed with exception: #{inspect(e)}")
+        raise e
     end
-  end
-
-  def get_business_key(process_data, application_prefix) do
-    values = Enum.map(application_prefix, &Map.get(process_data, &1))
-    Enum.reduce(values, &(&2 <> "-" <> &1))
   end
 end
