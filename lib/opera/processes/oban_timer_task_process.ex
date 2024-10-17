7641389c5bf6c1bfe940abeca45064096daf21d7
Chuck Irvine <chuck.irvine@icloud.com>
2024-10-17 16:35:33 -0500

    sync mozart bpm app changes

--- lib/opera/processes/oban_timer_task_process.ex
+++ lib/opera/processes/oban_timer_task_process.ex
@@ -1,17 +1,15 @@
 defmodule Opera.Processes.ObanTimerTaskProcess do
   use Mozart.BpmProcess
 
-  def_bpm_application("Oban Timer Process",
-    data: "foobar",
-    bk_prefix: "foobar")
+  def_bpm_application("Oban Timer Process")
 
   defprocess "Oban Timer Process" do
-    timer_task("Wait One Minute", duration: 30, function: :schedule_oban_task)
+    timer_task("Wait One Minute", duration: 60, function: :schedule_oban_task)
   end
 
   def schedule_oban_task(task_uid, process_uid, duration) do
     %{"task_id" => task_uid, "process_uid" => process_uid}
-    |> Opera.Processes.ObanTimerWorker.new(schedule_in: duration)
+    |> Opera.Workers.ObanTimerWorker.new(schedule_in: duration)
     |> Oban.insert()
   end
 end
